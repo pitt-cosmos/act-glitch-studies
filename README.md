4336848fa79fb954652f2a035d02e7250847e04f
Yilun Guan <zoom.aaron@gmail.com>
2018-04-23 12:08:52 -0400

    Add SpreadFilter to filter tracks based on mean spread
    - Modify readme with new dev guidelines

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ ACT Glitch studies to characterize all glitches and find FRBs hopefully!
 ## Instructions
 To have the best user experience, add the project folder to your pythonpath.
 
-## Conventions
+## Development Guidelines
 1. Class name use camel case like `GetTrackWithSpread`. 
-2. Method name use lowercase with understores like `get_tod_id`. 
+2. Method name use lowercase with underscores like `get_tod_id`. 
 3. Path always include `/` at the end. 
 4. Always run scripts in project root directory, like `python get_tracks/get_tracks.py`
 5. Organize independent codes into its subfolder instead of project root directory
-6. Script slurm_submit.py assumes your script has `sys.argv[1]` to be starting index, `sys.argv[2]` to be ending index
+6. Script `slurm_submit.py` assumes your script has `sys.argv[1]` to be starting index, `sys.argv[2]` to be ending index
+7. Inline comment starts after two spaces
+8. Class attributes use lowercase with underscores, starting with underscore like `_tod_id`
+9. Develop new features in new branches, and submit merge requests when tested working. 
--- eventloop/filters.py
+++ eventloop/filters.py
@@ -1,17 +1,18 @@
 from eventloop.base import Routine
+import numpy as np
 
 
 class DurationFilter(Routine):
     """An event filter based on the duration of events (set max duration)"""
     def __init__(self, max_duration=50):
         Routine.__init__(self)
-        self.max_duration = max_duration
+        self._max_duration = max_duration
         
     def execute(self):
         cosig = self.get_context().get_store().get("cosig")
         peaks = cosig['peaks']
         print '[INFO] Before: n_tracks = %d' % len(cosig['peaks'])
-        peaks_filtered = [peak for peak in peaks if peak[2] < self.max_duration]
+        peaks_filtered = [peak for peak in peaks if peak[2] < self._max_duration]
         cosig['peaks'] = peaks_filtered
         print '[INFO] After: n_tracks = %d' % len(cosig['peaks'])
         self.get_context().get_store().set("cosig", cosig)
@@ -21,24 +22,29 @@ class PixelFilter(Routine):
     """An event filter based on the number of pixels affected (set max n_pixels)"""
     def __init__(self, max_pixels=10):
         Routine.__init__(self)
-        self.max_pixels = max_pixels
+        self._max_pixels = max_pixels
         
     def execute(self):
         cosig = self.get_context().get_store().get("cosig")
         peaks = cosig['peaks']
         print '[INFO] Before: n_tracks = %d' % len(cosig['peaks'])
-        peaks_filtered = [peak for peak in peaks if peak[3] < self.max_pixels]
+        peaks_filtered = [peak for peak in peaks if peak[3] < self._max_pixels]
         cosig['peaks'] = peaks_filtered
         print '[INFO] After: n_tracks = %d' % len(cosig['peaks'])
         self.get_context().get_store().set("cosig", cosig)
 
         
 class SpreadFilter(Routine):
-    """An event filter based on the spread of the pixels affected (set max spread)"""
+    """A track filter based on the mean spread of the tracks (set max spread).
+       Dependency: [ GetTracks ]"""
     def __init__(self, max_spread=10):
         Routine.__init__(self)
-        self.max_spread = max_spread
+        self._max_spread = max_spread
         
     def execute(self):
-        cosig = self.get_context().get_store().get("cosig")
+        tracks = self.get_context().get_store().get("tracks")
+        # filter based on mean spread
+        tracks_new = [ track for track in tracks if np.mean(track[:,2]) < self._max_spread ]
+        self.get_context().get_store().set("tracks", tracks_new)
+
         
