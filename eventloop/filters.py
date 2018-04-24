c80a7c184349cac048db164e5d09d4b0467a79ba
Yilun Guan <zoom.aaron@gmail.com>
2018-04-23 23:54:00 -0400

    Add new filters to eventloop module
    - Add NCosigFilter to filter based on maximum cosigs
    - Add TrackLengthFilter to filter based on track lengths
    - Remove slurm folder which is no longer needed

--- eventloop/filters.py
+++ eventloop/filters.py
@@ -44,7 +44,35 @@ class SpreadFilter(Routine):
     def execute(self):
         tracks = self.get_context().get_store().get("tracks")
         # filter based on mean spread
-        tracks_new = [ track for track in tracks if np.mean(track[:,2]) < self._max_spread ]
+        tracks_new = [track for track in tracks if np.mean(track[:, 2]) < self._max_spread]
         self.get_context().get_store().set("tracks", tracks_new)
 
-        
+
+class NCosigFilter(Routine):
+    """A TOD filter based on the number of cosig (coincident signals)
+       (set max n_cosig)"""
+    def __init__(self, max_cosig):
+        Routine.__init__(self)
+        self._max_cosig = max_cosig
+
+    def execute(self):
+        cosig = self.get_context().get_store().get("cosig")
+        peaks = cosig['peaks']
+        if len(peaks) > self._max_cosig:
+            self.veto()  # halt subsequent routines
+
+
+class TrackLengthFilter(Routine):
+    """A track filter that select tracks up to a maximum lengths"""
+    def __init__(self, max_length):
+        Routine.__init__(self)
+        self._max_length = max_length
+
+    def execute(self):
+        tracks = self.get_context().get_store().get("tracks")
+        # filter tracks based on the length of tracks
+        tracks_new = [track for track in tracks if len(tracks) < self._max_length]
+        self.get_context().get_store().set("tracks", tracks_new)
+
+
+
--- slurm/readme.txt
+++
@@ -1,2 +0,0 @@
-This folder contains various slurm scripts. It is for pure organizat-
-ional convenience. All scripts should be run in the parent folder.
