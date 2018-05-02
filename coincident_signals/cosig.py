d5b7f0ee01692192354791bd46b7b6d800aad5ce
Yilun Guan <zoom.aaron@gmail.com>
2018-05-02 14:48:00 -0400

    Make subfolders package

--- coincident_signals/cosig.py
+++ coincident_signals/cosig.py
@@ -2,9 +2,9 @@ from eventloop.base import EventLoop, DataLoader
 from eventloop.routines import Logger
 
 loop = EventLoop()
-loop.add_tod_list("data/s17_pa3_list.txt")
+loop.add_tod_list("data/s17_pa4_sublist.txt")
 
-loop.add_routine(DataLoader(input_dir="outputs/s17_pa4_sublist/", postfix="cut", output_key="cut"))
+loop.add_routine(DataLoader(input_dir="outputs/s17_pa4_sublist/", output_key="cut"))
 loop.add_routine(Logger(input_key="cut"))
 
 loop.run(0, 2)
