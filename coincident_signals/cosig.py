e469846945f9b3de93f8b400de2f6bdffb4e80f0
Yilun Guan <zoom.aaron@gmail.com>
2018-05-02 21:38:51 -0400

    Migrate coincident_signals to EventLoop framework
    - Implement FindCosigs routine to find coincident signals
    - Implement FindEvents routine to find peaks

--- coincident_signals/cosig.py
+++ coincident_signals/cosig.py
@@ -1,10 +1,15 @@
 from eventloop.base import EventLoop, DataLoader
 from eventloop.routines import Logger
+from routines import RemoveMCE, TrimEdges, FindCosigs, FindEvents
 
 loop = EventLoop()
 loop.add_tod_list("data/s17_pa4_sublist.txt")
 
 loop.add_routine(DataLoader(input_dir="outputs/s17_pa4_sublist/", output_key="cut"))
-loop.add_routine(Logger(input_key="cut"))
+loop.add_routine(RemoveMCE(input_key="cut", output_key="cut"))
+loop.add_routine(TrimEdges(input_key="cut", output_key="cut"))
+loop.add_routine(FindCosigs(input_key="cut", output_key="cosig"))
+loop.add_routine(FindEvents(input_key="cosig", output_key="data"))
+loop.add_routine(Logger(input_key="data"))
 
 loop.run(0, 2)
--- coincident_signals/routines.py
+++ coincident_signals/routines.py
@@ -1,5 +1,7 @@
 from eventloop.base import Routine
-from eventloop.utils.cuts import remove_overlap_tod, trim_edge_cuts
+from eventloop.utils.cuts import remove_overlap_tod, trim_edge_cuts, merge_cuts, common_cuts
+from eventloop.utils.pixels import PixelReader
+import numpy as np
 
 
 class RemoveMCE(Routine):
@@ -39,6 +41,176 @@ class TrimEdges(Routine):
         # save cleaned cuts to data store
         cuts_data['cuts'] = clean_cuts
         self.get_store().set(self._output_key, cuts_data)
+
+
+class FindCosigs(Routine):
+    """A routine that compiles the coincident signals from cuts"""
+    def __init__(self, input_key="cuts", output_key="cosig", strict=True, polarized=False):
+        """
+        :param input_key: string
+        :param output_key: string
+        :param strict: boolean - Strict mode means each pixel must have 4 TES
+                       detectors. Loose mode means that each frequency has at
+                       least one TES detectors.
+        :param polarized: boolean - True means that we are looking for potentially
+                        polarized signals. False means that we only look for
+                        un-polarized signals.
+        """
+        Routine.__init__(self)
+        self._input_key = input_key
+        self._output_key = output_key
+        self._pr = None
+        self._strict = strict
+        self._polarized = polarized
+
+    def initialize(self):
+        self._pr = PixelReader()
+        if (not self._strict) and (not self._polarized):  # give a warning
+            print '[WARNING] Using loose mode for unpolarized signals may not be accurate'
+
+    def execute(self):
+        # retrieve all cuts
+        cuts_data = self.get_store().get(self._input_key)  # get saved cut data
+        cuts = cuts_data['cuts']
+        nsamps = cuts_data['nsamps']
+
+        # get all pixels
+        pixels = self._pr.get_pixels()
+
+        # initialize dictionary to store coincident signals
+        cosig = {}
+
+        # loop through pixels and find cosig for each pixel
+        for p in pixels:
+            dets_f1 = self._pr.get_f1(p)
+            dets_f2 = self._pr.get_f2(p)
+
+            if self._strict:  # strict mode, 4 TES have to be present
+                if len(dets_f1) == 2 and len(dets_f2) == 2:
+                    cuts_f1_A = cuts.cuts[dets_f1[0]]  # low freq, polarization A
+                    cuts_f1_B = cuts.cuts[dets_f1[1]]  # low freq, polarization B
+
+                    cuts_f2_A = cuts.cuts[dets_f2[0]]  # high freq, polarization A
+                    cuts_f2_B = cuts.cuts[dets_f2[1]]  # high freq, polarization B
+
+                    if self._polarized:  # if looking for polarized, glitch may occur in either polarization
+                        cuts_f1 = merge_cuts(cuts_f1_A, cuts_f1_B)  # polarized spikes may appear at either pol
+                        cuts_f2 = merge_cuts(cuts_f2_A, cuts_f2_B)
+
+                    else:  # if looking for unpolarized, glitch must occur in both polarizations
+                        cuts_f1 = common_cuts(cuts_f1_A, cuts_f1_B)  # unpolarized spikes appear in both pols
+                        cuts_f2 = common_cuts(cuts_f2_A, cuts_f2_B)
+
+                    cosig[str(p)] = common_cuts(cuts_f1, cuts_f2)  # store coincident signals by pixel id
+
+            else:  # loose mode, at least one TES has to be present each freq
+                if len(dets_f1) == 2 and len(dets_f2) == 2:
+                    cuts_f1_A = cuts.cuts[dets_f1[0]]  # low freq, polarization A
+                    cuts_f1_B = cuts.cuts[dets_f1[1]]  # low freq, polarization B
+
+                    cuts_f2_A = cuts.cuts[dets_f2[0]]  # high freq, polarization A
+                    cuts_f2_B = cuts.cuts[dets_f2[1]]  # high freq, polarization B
+
+                    if self._polarized:  # if looking for polarized, glitch may occur in either polarization
+                        cuts_f1 = merge_cuts(cuts_f1_A, cuts_f1_B)  # polarized spikes may appear at either pol
+                        cuts_f2 = merge_cuts(cuts_f2_A, cuts_f2_B)
+
+                    else:  # if looking for unpolarized, glitch must occur in both polarizations
+                        cuts_f1 = common_cuts(cuts_f1_A, cuts_f1_B)  # unpolarized spikes appear in both pols
+                        cuts_f2 = common_cuts(cuts_f2_A, cuts_f2_B)
+
+                    cosig[str(p)] = common_cuts(cuts_f1, cuts_f2)  # store coincident signals by pixel id
+
+                elif len(dets_f1) == 1 and len(dets_f2) == 2:
+                    cuts_f1 = cuts.cuts[dets_f1[0]]  # low freq, polarization A
+
+                    cuts_f2_A = cuts.cuts[dets_f2[0]]  # high freq, polarization A
+                    cuts_f2_B = cuts.cuts[dets_f2[1]]  # high freq, polarization B
+
+                    if self._polarized:  # if looking for polarized, glitch may occur in either polarization
+                        cuts_f2 = merge_cuts(cuts_f2_A, cuts_f2_B)
+
+                    else:  # if looking for unpolarized, glitch must occur in both polarizations
+                        cuts_f2 = common_cuts(cuts_f2_A, cuts_f2_B)
+
+                    cosig[str(p)] = common_cuts(cuts_f1, cuts_f2)  # store coincident signals by pixel id
+
+                elif len(dets_f1) == 2 and len(dets_f2) == 1:
+                    cuts_f1_A = cuts.cuts[dets_f1[0]]  # low freq, polarization A
+                    cuts_f1_B = cuts.cuts[dets_f1[1]]  # low freq, polarization B
+
+                    cuts_f2 = cuts.cuts[dets_f2[0]]  # high freq, polarization A
+
+                    if self._polarized:  # if looking for polarized, glitch may occur in either polarization
+                        cuts_f1 = merge_cuts(cuts_f1_A, cuts_f1_B)
+
+                    else:  # if looking for unpolarized, glitch must occur in both polarizations
+                        cuts_f1 = common_cuts(cuts_f1_A, cuts_f1_B)
+
+                    cosig[str(p)] = common_cuts(cuts_f1, cuts_f2)  # store coincident signals by pixel id
+
+                elif len(dets_f1) == 1 and len(dets_f2) == 1:
+                    cuts_f1 = cuts.cuts[dets_f1[0]]  # low freq, polarization A
+                    cuts_f2 = cuts.cuts[dets_f2[0]]  # high freq, polarization A
+
+                    cosig[str(p)] = common_cuts(cuts_f1, cuts_f2)  # store coincident signals by pixel id
+
+        self.get_store().set(self._output_key, cosig)  # save the coincident signals under the output_key
+        self.get_store().set("nsamps", nsamps)  # save the number of sampling points
+
+
+class FindEvents(Routine):
+    """A routine to find events that cause multiple cosigs
+    dependency: FindCosigs"""
+    def __init__(self, input_key="cosig", output_key="events"):
+        Routine.__init__(self)
+        self._input_key = input_key
+        self._output_key = output_key
+        self._cosig = None
+        self._nsamps = None
+
+    def find_peaks(self, hist):
+        """Find peaks in the histogram corresponding to physical events
+        :param hist: histogram of coincident signals
+        :return: list: [start_time, end_time, duration, n_pixels_affected]
+        """
+        last = 0
+        peaks = []
+        for i in range(self._nsamps):
+            if hist[i] > 0 and last == 0:
+                peak_start = i
+            if hist[i] == 0 and last > 0:
+                peak_end = i
+                peak_amp = max(hist[peak_start:peak_end])
+                peak_duration = peak_end - peak_start
+                peaks.append([peak_start, peak_end, peak_duration, peak_amp])
+            last = hist[i]
+        return peaks
+
+    def execute(self):
+        self._cosig = self.get_store().get(self._input_key)
+        self._nsamps = self.get_store().get("nsamps")  # get nsamps from FindCosigs, not very graceful
+
+        # generate a histogram of cosigs
+        cosig_hist = np.zeros(1, self._nsamps)
+
+        for pixel, cuts in self._cosig:  # loop through key and value pair in cosig dict
+            mask = cuts.get_mask(nsamps=self._nsamps)
+            cosig_hist += mask
+
+        cosig_peaks = self.find_peaks(cosig_hist)
+
+        # export the coincident signals and the peaks found in a combined dictionary
+        cosig_combined = {
+            'coincident_signals': self._cosig,
+            'peaks': cosig_peaks
+        }
+        self.get_store().set(self._output_key, cosig_combined)
+
+
+
+
+
 
 
 
