1932af55741261ccf2e247a33b318a3791f40afc
Yilun Guan <zoom.aaron@gmail.com>
2018-05-03 11:56:18 -0400

    Add a TODInfoLoader routine

--- eventloop/tod.py
+++ eventloop/tod.py
@@ -1,6 +1,7 @@
 import moby2
 from moby2.scripting import get_filebase
 from base import Routine
+import pandas as pd
 
 
 class TODLoader(Routine):
@@ -29,4 +30,26 @@ class TODLoader(Routine):
         print '[INFO] TOD loaded'
         self.get_store().set(self._output_key, tod_data)  # save tod_data in memory for routines to process
 
+
+class TODInfoLoader(Routine):
+    """A routine to load TODInfo"""
+    def __init__(self, season='2016', array='AR3'):
+        Routine.__init__(self)
+        self._tod_info = None
+        self._season = season
+        self._array = array
+
+    def initialize(self):
+        self._tod_info = pd.read_csv('data/s16_pa3_tod_info.csv', header=None)
+        self._tod_info.columns = ['TOD', 'hour', 'altitude', 'azimuth', 'PWV', 'cut_status', 'field']
+
+        def get_tod_info(tod_name, field):
+            tod = self._tod_info[self._tod_info['TOD']==tod_name]
+            if len(tod) != 0:  # if an entry is found
+                return tod[field]
+            else:  # if no entry is found
+                print '[WARNING] No TOD found with name: %s' % tod_name
+
+        # A hack to store functions in shared datastore. Prefix with f_ to denote this fact. 
+        self.get_store().set("f_get_tod_info", get_tod_info)
 
--- eventloop/utils/tods.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('data/s16_pa3_tod_info.csv', header=None)
-data.columns = ['TOD', 'hour', 'altitude', 'azimuth', 'PWV', 'cut_status', 'field']
-
-print data
