c1f9dfca537951b558110c8d88c495aadbb6daa0
Nicholas Wertz <njw27@feynman.princeton.edu>
2018-05-16 10:20:13 -0400

    Updats and a new interesting glitches file with high correlations

--- correlation/plotter.py
+++ correlation/plotter.py
@@ -68,7 +68,7 @@ class PlotGlitches(Routine):
                 y = timeseries(pid,start_time,end_time)[1]
 
                 plt.title('Pixels affected from ' +str(start_time)+ '-' + str(end_time)+ ' at 90 GHz')
-                plt.xlabel('TOD track: 10000')  # CHANGE TOD TRACK NAME
+                plt.xlabel('TOD track: 14594')  # CHANGE TOD TRACK NAME
                 plt.plot(x,y,'.-')
             
             plt.show()
@@ -79,7 +79,7 @@ class PlotGlitches(Routine):
         """
         cs = cuts['coincident_signals']
 
-        event = [204396,204403,7,6]
+        event = [210816, 210869, 53, 23]
         stime = event[0]
         etime = event[1]
         pixels = pixels_affected_in_event(cs, event)
