06f2a33bf77af74428a330083f6c936702c296e9
Yilun Guan <zoom.aaron@gmail.com>
2018-05-18 14:32:22 -0400

    Add __init__ to plot_events so NPixelFilter can be used elsewhere

--- cr_hist/cr_hist.py
+++ cr_hist/cr_hist.py
@@ -2,12 +2,14 @@ from todloop.base import TODLoop
 from todloop.tod import TODInfoLoader
 from todloop.base import DataLoader
 from routines import NPixelStudy, CRHourStudy, CRPWVStudy
+from plot_events.routines import NPixelFilter
 
 
 loop = TODLoop()
 loop.add_tod_list("../data/s16_pa3_list.txt")
 loop.add_routine(DataLoader(input_dir="../outputs/s16_pa3_list/events/", output_key="events"))
 
+loop.add_routine(NPixelFilter(min_pixels=30, max_pixels=100, input_key="events", output_key="events"))
 loop.add_routine(NPixelStudy())
 loop.add_routine(TODInfoLoader(output_key="tod_info"))
 loop.add_routine(CRHourStudy())
