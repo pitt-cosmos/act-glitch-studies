ee03bb504869cb97553feb174a52dd733477165f
Yilun Guan <zoom.aaron@gmail.com>
2018-05-18 12:55:59 -0400

    Add plot_event module to plot all events that pass through a filter

---
+++ plot_events/plot_events.py
@@ -0,0 +1,16 @@
+from todloop.base import TODLoop
+from todloop.tod import TODLoader
+from todloop.base import DataLoader
+from routines import PlotEvents, NPixelFilter
+from calibration.routines import FixOpticalSign, CalibrateTOD
+
+loop = TODLoop()
+loop.add_tod_list("../data/s16_pa3_list.txt")
+loop.add_routine(DataLoader(input_dir="../outputs/s16_pa3_list/events/", output_key="events"))
+loop.add_routine(NPixelFilter(min_pixels=150, max_pixels=250, input_key="events", output_key="events"))
+loop.add_routine(TODLoader(output_key="tod_data"))
+loop.add_routine(FixOpticalSign(input_key="tod_data", output_key="tod_data"))
+loop.add_routine(CalibrateTOD(input_key="tod_data", output_key="tod_data"))
+loop.add_routine(PlotEvents(tod_key="tod_data", event_key="events"))
+
+loop.run(0,5000)
---
+++ plot_events/routines.py
@@ -0,0 +1,115 @@
+import matplotlib
+matplotlib.use("TKAgg")
+import numpy as np
+import matplotlib.pyplot as plt
+from todloop.routines import Routine
+from todloop.utils.pixels import PixelReader
+
+
+class PlotEvents(Routine):
+    """A routine that plot events"""
+    def __init__(self, event_key, tod_key):
+        Routine.__init__(self)
+        self._event_key = event_key
+        self._tod_key = tod_key
+        self._pr = None
+
+    def initialize(self):
+        self._pr = PixelReader()
+
+    def execute(self):
+        print '[INFO] Plotting glitches ...'
+        tod_data = self.get_store().get(self._tod_key)  # retrieve tod_data
+        events = self.get_store().get(self._event_key)  # retrieve tod_data
+        
+        def timeseries(pixel_id, s_time, e_time, buffer=10):
+
+            start_time = s_time - buffer
+            end_time = e_time + buffer
+
+            a1, a2 = self._pr.get_f1(pixel_id)
+            b1, b2 = self._pr.get_f2(pixel_id)
+            d1, d2 = tod_data.data[a1], tod_data.data[a2]
+            d3, d4 = tod_data.data[b1], tod_data.data[b2]
+
+            # try to remove the mean from start_time to end_time
+            d1 -= np.mean(d1[start_time:end_time])
+            d2 -= np.mean(d2[start_time:end_time])
+            d3 -= np.mean(d3[start_time:end_time])
+            d4 -= np.mean(d4[start_time:end_time])
+            
+            time = tod_data.ctime - tod_data.ctime[0]
+            time = time[start_time:end_time]
+            
+            d_1 = d1[start_time:end_time]
+            d_2 = d2[start_time:end_time]
+            d_3 = d3[start_time:end_time]
+            d_4 = d4[start_time:end_time]
+
+            """
+            UNCOMMENT TO PLOT FOUR CORRESPONDING PIXELS WITH HI-LO FREQ
+            plt.plot(time,d_1, '.-', label=str(a1) + ' 90 GHz')
+            plt.plot(time, d_2, '.-', label=str(a2) + ' 90 GHz')
+            plt.plot(time, d_3, '.-', label=str(b1) + ' 150 GHz')
+            plt.plot(time, d_4, '.-', label=str(b2) + ' 150 GHz')
+            plt.legend(title='Detector UID')
+            plt.show()
+            """
+
+            return time, d_1
+
+
+        """
+        PLOTTING FUNCTION
+        Plot all pixels affected given an array of pixel ids
+        and a starting time and ending time
+      
+        """
+        def plotter(pixels,start_time,end_time):
+                        
+            for pid in pixels:
+               
+                x = timeseries(pid,start_time,end_time)[0]
+                y = timeseries(pid,start_time,end_time)[1]
+
+                plt.title('Pixels affected from ' +str(start_time)+ '-' + str(end_time)+ ' at 90 GHz')
+                plt.xlabel('TOD_ID: %d\t TOD_NAME: %s' % (self.get_id(), self.get_name()))  # CHANGE TOD TRACK NAME
+                plt.plot(x,y,'.-')
+            
+            plt.show()
+
+            
+        """
+        ALL EVENTS
+        Plot all pixels affected in a event one by one for all events
+        """
+        
+        for event in events:
+            pixels_affected = event['pixels_affected']
+            start_time = event['start']
+            end_time = event['end']
+            plotter(pixels_affected, start_time, end_time)
+
+
+class NPixelFilter(Routine):
+    def __init__(self, min_pixels=0, max_pixels=100, input_key="events", output_key="events"):
+        """Scripts that run during initialization of the routine"""
+        Routine.__init__(self)
+        self._min_pixels = min_pixels
+        self._max_pixels = max_pixels
+        self._input_key = input_key
+        self._output_key = output_key
+
+    def execute(self):
+        """Scripts that run for each TOD"""
+        events = self.get_store().get(self._input_key)
+        events_filtered = [event for event in events if self._min_pixels <= event['number_of_pixels'] < self._max_pixels]
+        
+        # if no events left, skip TOD
+        if len(events_filtered) == 0:
+            self.veto()  # skip subsequent routines
+            return
+        else:
+            print '[INFO] Found %d events' % len(events_filtered)
+            self.get_store().set(self._output_key, events_filtered)
+        
