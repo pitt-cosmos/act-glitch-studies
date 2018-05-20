8c08b36ec0afb74f529c932c6309a483e62ec96e
Yilun Guan <zoom.aaron@gmail.com>
2018-05-20 09:56:09 -0400

    Adapt LoadRaDec routine to events_v2

--- plot_events/routines.py
+++ plot_events/routines.py
@@ -115,10 +115,34 @@ class NPixelFilter(Routine):
             self.veto()  # skip subsequent routines
             return
         else:
-            print '[INFO] Found %d events' % len(events_filtered)
+            print '[INFO] Events passed %d / %d' % (len(events_filtered), len(events))
+            self.get_store().set(self._output_key, events_filtered)
+
+            
+class CoeffFilter(Routine):
+    def __init__(self, min_coeff=0.8, max_coeff=1, input_key="events", output_key="events"):
+        """Filter events based on coefficient"""
+        Routine.__init__(self)
+        self._min_coeff = min_coeff
+        self._max_coeff = max_coeff
+        self._input_key = input_key
+        self._output_key = output_key
+
+    def execute(self):
+        """Scripts that run for each TOD"""
+        events = self.get_store().get(self._input_key)
+        events_filtered = [event for event in events if self._min_coeff <= event['coefficient'] < self._max_coeff]
+        
+        # if no events left, skip TOD
+        if len(events_filtered) == 0:
+            self.veto()  # skip subsequent routines
+            return
+        else:
+            print '[INFO] Events passed: %d / %d' % (len(events_filtered), len(events))
             self.get_store().set(self._output_key, events_filtered)
         
 
+            
 class LoadRaDec(Routine):
     """A routine that loads the information about RA and DEC for each events"""
     def __init__(self, event_key="events", tod_key="tod_data", output_key="events"):
@@ -145,12 +169,13 @@ class LoadRaDec(Routine):
         
         new_events = []
         for event in events:  # loop over each event
-            start = event['start']
-            end = event['end']
+            # start = event['start']
+            # end = event['end']
             # ra, dec = moby2.pointing.get_coords(tod_data.ctime[start:end], tod_data.az[start:end], tod_data.alt[start:end], focal_plane=focal_plane)
-            ra, dec = moby2.pointing.get_coords(tod_data.ctime[start:end], tod_data.az[start:end], tod_data.alt[start:end])
-            ref_index = int(len(ra)/2)  # use the middle index as a reference
-            event['ra'] = ra[ref_index]
-            event['dec'] = dec[ref_index]
+            # ra, dec = moby2.pointing.get_coords(tod_data.ctime[start:end], tod_data.az[start:end], tod_data.alt[start:end])
+            ra, dec = moby2.pointing.get_coords([event['ctime']], [event['az']], [event['alt']])
+            # ref_index = int(len(ra)/2)  # use the middle index as a reference
+            event['ra'] = ra[0]  # ra / dec is a vector with one element
+            event['dec'] = dec[0]
             new_events.append(event)
         self.get_store().set(self._output_key, new_events)
