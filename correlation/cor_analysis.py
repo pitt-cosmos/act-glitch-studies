a1fa07b798f38fc9687bb123b850cb00d12094b4
Nicholas Wertz <njw27@feynman.princeton.edu>
2018-05-21 13:25:09 -0400

    commiting to pull

--- correlation/cor_analysis.py
+++ correlation/cor_analysis.py
@@ -31,7 +31,7 @@ loop.add_routine(CalibrateTOD(input_key="tod_data",output_key="tod_data"))
 #PLOT A GLITCH (modify plotter.py to plot the specific event)
 loop.add_routine(PlotGlitches(tod_key="tod_data", cosig_key="cuts"))
 
-loop.run(10000,10001)
+loop.run(1956, 1957)
 
 
 
