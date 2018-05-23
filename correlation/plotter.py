23d70b5053a187670cc2bf6c080030be36100411
Janvi Madhani <janvimadhani@gmail.com>
2018-05-23 13:49:40 -0400

    trying to pull

--- correlation/plotter.py
+++ correlation/plotter.py
@@ -81,8 +81,7 @@ class PlotGlitches(Routine):
         """
         cs = cuts['coincident_signals']
 
-        event =  [253309, 253314, 5, 3]            
-
+        event = [204918, 204922, 4, 1]
         stime = event[0]
         etime = event[1]
         pixels = pixels_affected_in_event(cs, event)
