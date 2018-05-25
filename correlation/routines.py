560c26cb54203db2ee3d6c228e51ed90c1f0b739
Janvi Madhani <janvimadhani@gmail.com>
2018-05-25 11:46:33 -0400

    Yilun's new correlation function

--- correlation/routines.py
+++ correlation/routines.py
@@ -112,7 +112,7 @@ class CorrelationFilter(Routine):
             return x, y
 
         def correlation(x1,x2,y1,y2):
-            
+            """
             f1 = interp1d(x1,y1)
             f2 = interp1d(x2,y2)
 
@@ -125,18 +125,29 @@ class CorrelationFilter(Routine):
             y1new = f1(x1new)
             y2new = f2(x2new)
             """
+            """
             NUMPY CORRELATION ROUTINE
             """
             #m_coeff = np.corrcoef(y1new,y2new)[0][1]
-
+            """
             a = y1new
             b = y2new
             a = (a - np.mean(a)) / (np.std(a) * len(a))
             b = (b - np.mean(b)) / (np.std(b))
             c = np.correlate(a, b, 'full')
             m_coeff = np.max(abs(c))
-            
-            return m_coeff
+            """
+            ts1 = y1
+            ts2 = y2 
+            l1 = len(ts1)
+            l2 = len(ts2)
+            if l1 < l2:
+                n = l1
+                return max([np.corrcoef(ts1, ts2[i:n+i])[0][1] for i in range(0, l2-l1)])
+            else:
+                n = l2
+                return max([np.corrcoef(ts1[i:n+i], ts2)[0][1] for i in range(0, l1-l2)])
+            #return m_coeff
 
             
             """
