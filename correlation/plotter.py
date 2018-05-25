d5aec41f8e0d3d5278c0f3f6a1db0e9e02100a37
Janvi Madhani <janvimadhani@gmail.com>
2018-05-25 10:44:42 -0400

    You can keep plotting a graph with raw_inputs w/o switching b/w programs

--- correlation/plotter.py
+++ correlation/plotter.py
@@ -67,17 +67,65 @@ class PlotGlitches(Routine):
             plt.figure(figsize=(8,8))
             gridspec.GridSpec(11,11)
             def plotter(pixels,start_time,end_time):
-                plt.subplot2grid((11,11), (0,0), colspan=11, rowspan=3)            
+                
+                plt.subplot2grid((11,11), (0,0), colspan=11, rowspan=3)                
                 for pid in pixels:
                
                     x = timeseries(pid,start_time,end_time)[0]
                     y = timeseries(pid,start_time,end_time)[1]
                 
                     plt.plot(x,y,'.-')
-                
                     plt.title('Pixels affected from ' +str(start_time)+ '-' + str(end_time)+ ' at 90 GHz')
-
                     plt.xlabel('TOD track: 7140')  # CHANGE TOD TRACK NAME
+
+                pix_max_amps = []
+                pix_max_x = []
+                pix_max_y = []
+                pix_location_row = []
+                pix_location_col = []
+                pix_all_amps = []
+                x1, y1 = self._pr.get_x_y_array()
+                plt.subplot2grid((11,11), (4,0), colspan=7, rowspan=7)
+                plt.plot(x1,y1,'r.')
+
+                for pid in pixels:
+
+                    #print '[INFO] Pixel #', pid, 'at', self._pr.get_row_col(pid)
+                    pixel_max_amp = np.amax(timeseries(pid,stime,etime)[1])
+                    #print '[INFO] Maximum Amplitude of Pixel #', pid, 'is', pixel_max_amp
+                    x, y = self._pr.get_x_y(pid)
+                    pix_max_amps.append(pixel_max_amp)
+                    pix_max_x.append(x)
+                    pix_max_y.append(y)
+                    a1, a2 = self._pr.get_f1(pid)
+                    b1, b2 = self._pr.get_f2(pid)    
+                    pix_location_row.append(self._pr.get_row_col(a1)[0]) 
+                    pix_location_col.append(self._pr.get_row_col(a1)[1])
+                    pix_location_row.append(self._pr.get_row_col(a2)[0])             
+                    pix_location_col.append(self._pr.get_row_col(a2)[1])
+                    pix_location_row.append(self._pr.get_row_col(b1)[0])             
+                    pix_location_col.append(self._pr.get_row_col(b1)[1])
+                    pix_location_row.append(self._pr.get_row_col(b2)[0])             
+                    pix_location_col.append(self._pr.get_row_col(b2)[1])
+                    pix_all_amps.append(timeseries(pid,stime,etime)[1])
+                
+                max_alpha = np.amax(pix_max_amps)
+        
+                for n in np.arange(0,len(pix_max_amps)):
+                    plt.plot(pix_max_x[n],pix_max_y[n], 'b.', alpha=0.8*(pix_max_amps[n]/max_alpha), markersize=20)
+                
+                plt.subplot2grid((11,11), (6,8), colspan=4, rowspan=4)
+                plt.plot(pix_location_col,pix_location_row, 'b.', alpha = 1, markersize=10)
+                plt.title('Location of Affected Pixels',fontsize=10)
+                plt.xticks(np.arange(min(pix_location_col)-1, max(pix_location_col)+2, 1.0))
+                plt.xlabel('Column', fontsize=8)
+                plt.yticks(np.arange(min(pix_location_row)-1, max(pix_location_row)+2, 1.0))
+                plt.ylabel('Row', fontsize=8)
+                plt.xticks(fontsize=5)
+                plt.yticks(fontsize=5)
+                plt.grid(color='k', linewidth=1)
+                plt.show() 
+                
 
             """
             SPECIFIC EVENT
@@ -92,58 +140,18 @@ class PlotGlitches(Routine):
             pixels = pixels_affected_in_event(cs, event)
             print '[INFO Plotting Glitch...'
             plotter(pixels, stime, etime)
-                        
-            pix_max_amps = []
-            pix_max_x = []
-            pix_max_y = []
-            pix_location_row = []
-            pix_location_col = []
-            pix_all_amps = []
-            x, y = self._pr.get_x_y_array()
-            plt.subplot2grid((11,11), (4,0), colspan=7, rowspan=7)
-            plt.plot(x,y,'r.')
-        
-            for pid in pixels:
-                #print '[INFO] Pixel #', pid, 'at', self._pr.get_row_col(pid)
-                pixel_max_amp = np.amax(timeseries(pid,stime,etime)[1])
-                #print '[INFO] Maximum Amplitude of Pixel #', pid, 'is', pixel_max_amp
-                x, y = self._pr.get_x_y(pid)
-                pix_max_amps.append(pixel_max_amp)
-                pix_max_x.append(x)
-                pix_max_y.append(y)
-                a1, a2 = self._pr.get_f1(pid)
-                b1, b2 = self._pr.get_f2(pid)    
-                pix_location_row.append(self._pr.get_row_col(a1)[0]) 
-                pix_location_col.append(self._pr.get_row_col(a1)[1])
-                pix_location_row.append(self._pr.get_row_col(a2)[0])             
-                pix_location_col.append(self._pr.get_row_col(a2)[1])
-                pix_location_row.append(self._pr.get_row_col(b1)[0])             
-                pix_location_col.append(self._pr.get_row_col(b1)[1])
-                pix_location_row.append(self._pr.get_row_col(b2)[0])             
-                pix_location_col.append(self._pr.get_row_col(b2)[1])
-                pix_all_amps.append(timeseries(pid,stime,etime)[1])
-           
-                #            print '[INFO] Pixel #', pid, 'has amplitude sum', np.sum(timeseries(pid,stime,etime)[1])
-    
-                #print '[INFO] Sum of all detector amplitudes from ', stime, '-', etime, 'is', np.sum(pix_all_amps)
-       
-            max_alpha = np.amax(pix_max_amps)
-        
-            for n in np.arange(0,len(pix_max_amps)):
-                plt.plot(pix_max_x[n],pix_max_y[n], 'b.', alpha=0.8*(pix_max_amps[n]/max_alpha), markersize=20)
-        
-        
-            plt.subplot2grid((11,11), (6,8), colspan=4, rowspan=4)
-            plt.plot(pix_location_col,pix_location_row, 'b.', alpha = 1, markersize=10)
-            plt.title('Loctaion of Affected Pixels',fontsize=10)
-            plt.xticks(np.arange(min(pix_location_col)-1, max(pix_location_col)+2, 1.0))
-            plt.xlabel('Column', fontsize=8)
-            plt.yticks(np.arange(min(pix_location_row)-1, max(pix_location_row)+2, 1.0))
-            plt.ylabel('Row', fontsize=8)
-            plt.xticks(fontsize=5)
-            plt.yticks(fontsize=5)
-            plt.grid(color='k', linewidth=1)
-            plt.show() 
 
+            y_n = ' '
+            
+            while y_n != 'n':
+                y_n = raw_input ("Would you like to plot another event? Enter y/n...")
+                if y_n == 'y':
+                    e= raw_input('Please copy the event you would like to plot:')
+                    event = json.loads(e)
+                    stime = event[0]
+                    etime = event[1]
+                    pixels = pixels_affected_in_event(cs, event)
+                    print '[INFO Plotting Glitch...'
+                    plotter(pixels, stime, etime)
         else:
             print 'No plot will be displayed!'
