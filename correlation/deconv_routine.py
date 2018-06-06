a6066e228a0a4829b8e07d77182acb98299234fb
Janvi Madhani <janvimadhani@gmail.com>
2018-06-06 10:31:31 -0400

    changes to switch from moby2 to TODLoop

--- correlation/deconv_routine.py
+++ correlation/deconv_routine.py
@@ -3,36 +3,35 @@ matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
 import numpy as np
 import moby2
-from todloop.base import Routine 
+from todloop.base import Routine, TODLoop
 from moby2.instruments import actpol
 from moby2.scripting import get_filebase
 from moby2.detectors.stuff import TimeConstants
 
 class Deconvolution(Routine):
-    def __init__(self,input_key,output_key):
+    def __init__(self,tod_key,output_key):
         Routine.__init__(self)
-        self._input_key= input_key
+        TODLoop.__init__(self)
+        self._tod_key = tod_key 
         self._output_key = output_key
         
     def execute(self):
+        
         def tconst_filter(freq,tau):
             """
             Return the fourier space representation of the effect of 
             detector time constants, for the given frequencies
             """
             return 1/(2*np.pi*1j*freq*tau+1)
-        tid = self._input_key
-        db = actpol.TODDatabase()
-        ids = db.select_tods()
-        deconv_time = []
+        tod_name = self.get_name()
+        tod_string = str(tod_name)
+        data = self.get_store().get(self._tod_key)
+        tc = TimeConstants.read_from_path('/mnt/act3/users/spho/2016/TimeCo\
+nstantsperTOD_170718/pa2/' + tod_string[:5] + '/' + tod_string + '.tau')####array2/3
+       
         for i in range(1015):
-            tod_name = ids[tid].basename
-            tod_string = str(tod_name)
-            fb = get_filebase()
-            tod_dir = fb.filename_from_name(tod_name,single=True)
-            data = moby2.scripting.get_tod({'filename': tod_dir, 'repair_pointing':True})
-            tc = TimeConstants.read_from_path('/mnt/act3/users/spho/2016/TimeConstantsperTOD_170718/pa2/' + tod_string[:5] + '/' + tod_string + '.tau')
-            tod = moby2.tod.detrend_tod(data)
+            tod = moby2.tod.detrend_tod(data) ####
+            d_tod = data.data[i] ####
             ftod = np.fft.rfft(d_tod)
             nsamp = len(d_tod)
             srate = 400 #Hz
@@ -42,7 +41,7 @@ class Deconvolution(Routine):
             ftod /= resp
             deconv_tod = np.fft.irfft(ftod)
             deconv_data = {
-                'tod' = tid,
+                'tod' = tod_name,
                 'id' = i,
                 'data' = deconv_tod
                 }
