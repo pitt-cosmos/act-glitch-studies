38b8f4a4e66063da20210638b4dc66cda7723796
Janvi Madhani <janvimadhani@gmail.com>
2018-08-02 11:36:52 -0400

    Added TODSelector instead of loop.run

--- covered_tods/covered_tods.py
+++ covered_tods/covered_tods.py
@@ -1,11 +1,12 @@
 from todloop.routines import DataLoader
-from todloop.tod import TODLoader, TODInfoLoader
+from todloop.tod import TODLoader, TODInfoLoader, TODSelector
 from todloop.base import TODLoop
 from routines import PlotGlitches
 from calibration.routines import FixOpticalSign, CalibrateTOD
 
-tod_id = 1300
+
 loop = TODLoop()
+
 loop.add_tod_list("../data/covered_tods.txt")
 
 loop.add_routine(DataLoader(input_dir="../outputs/s16_pa3_list/cosig/", output_key="cuts"))
@@ -13,6 +14,5 @@ loop.add_routine(TODLoader(output_key="tod_data"))
 loop.add_routine(FixOpticalSign(input_key="tod_data", output_key="tod_data"))
 loop.add_routine(CalibrateTOD(input_key="tod_data",output_key="tod_data"))
 loop.add_routine(PlotGlitches(tag=tod_id,tod_key="tod_data", cosig_key="cuts"))
-loop.run(tod_id,tod_id + 1)
-
-
+#Equivalent of loop.run...?
+loop.TODSelector("../data/covered_tods.txt")
