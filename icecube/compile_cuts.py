202bf5a5d2d29787957d3f4b1056b72c1e99dfcd
Yilun Guan <zoom.aaron@gmail.com>
2018-08-21 18:35:25 -0400

    Add sample scripts for icecube study

---
+++ icecube/compile_cuts.py
@@ -0,0 +1,26 @@
+from todloop.tod import TODLoader, TODInfoLoader
+from todloop.base import TODLoop
+from todloop.routines import Logger
+from routines import CompileCuts
+
+import sys
+
+loop = TODLoop()
+loop.add_tod_list("data/s17_icecube_list.txt")
+
+glitchp = {
+    'nSig': 10, 
+    'tGlitch' : 0.007, 
+    'minSeparation': 30, 
+    'maxGlitch': 50000, 
+    'highPassFc': 6.0, 
+    'buffer': 0
+}
+
+loop.add_routine(TODLoader(output_key="tod_data"))
+#loop.add_routine(TODInfoLoader(output_key="tod_info"))
+loop.add_routine(CompileCuts(input_key="tod_data", glitchp=glitchp, output_dir="outputs/s17_icecube_list/"))
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+loop.run(start, end)
