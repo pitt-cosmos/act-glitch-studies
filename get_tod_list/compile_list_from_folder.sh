1b0b5ec92383a6c181b85ef37b0dcc572440858d
Yilun Guan <zoom.aaron@gmail.com>
2018-12-01 11:39:03 -0500

    Remove get_tod_list scripts

--- get_tod_list/compile_list_from_folder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ls /mnt/act6/actpol/data/season5/merlin/15022 | grep ar5 | cut -d'.' -f-3 | while read line
-do
-echo /mnt/act6/actpol/data/season5/merlin/15022/$line
-done > s17_pa5_sublist.txt
